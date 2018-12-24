a0c96e966ba79351a1478684d94fc044ff58dc31
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-24 01:07:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
     @recipe.cook_time = params[:cook_time]
     @recipe.save
 
-    redirect "/recipes/#{recipe.id}"
+    redirect "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
