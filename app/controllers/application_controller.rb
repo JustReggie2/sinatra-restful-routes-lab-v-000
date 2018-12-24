536b391f40b207da6896a8adf668c322fc6a8b33
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-12-24 00:40:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,12 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  get 'recipes/:id/edit' do
+    @recipe = Recipe.find_by(params[:id])
+
+    erb :edit
+  end
+
   get '/recipes/:id' do
     @recipe = Recipe.find_by(params[:id])
 
@@ -31,4 +37,20 @@ class ApplicationController < Sinatra::Base
     redirect "/recipes/#{recipe.id}"
   end
 
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.delete
+
+    redirect '/recipes'
+  end
 end
